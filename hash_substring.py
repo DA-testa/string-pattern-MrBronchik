2aec8d7d2e347dff685f12cfeaa26cd29806cd36
MrBronchik <85889573+MrBronchik@users.noreply.github.com>
2023-04-17 00:22:07 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,58 +1,59 @@
-import os
+# python3
 
 def read_input():
-    # read the input choice
-    choice = input().strip().lower()
-    if choice == 'i':
-        # read from keyboard
-        pattern = input().strip()
-        text = input().strip()
-    elif choice == 'f':
-        # read from file
-        filename = input().strip()
-        try:
-            with open(os.path.join('tests', filename), 'r') as f:
-                # check if the file has at least two lines
-                lines = [line.strip() for line in f.readlines()]
-                if len(lines) < 2:
-                    raise ValueError('Input file is not properly formatted')
-                pattern = lines[0]
-                text = lines[1]
-        except FileNotFoundError:
-            print("Error: file not found")
-            sys.exit(1)
-        except ValueError as e:
-            print(f"Error: {str(e)}")
-            sys.exit(1)
+    # this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
+    
+    # after input type choice
+    # read two lines 
+    # first line is pattern 
+    # second line is text in which to look for pattern 
+    
+    # return both lines in one return
+    
+    # this is the sample return, notice the rstrip function
+    t_input=input().strip().upper()
+    if t_input=="I":
+        pattern=input().strip()
+        text=input().strip()
+    elif t_input=="F":
+        filename='tests/06'
+        with open(filename) as file:
+            pattern=file.readline().strip()
+            text=file.readline().strip()
+            
     else:
-        print("Error: invalid input choice")
-        sys.exit(1)
-    return pattern, text
-
+        exit()
+    return t_input, pattern,text          
+    
 
 def print_occurrences(output):
+    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+def get_occurrences( t_input,pattern, text):
+    # this function should find the occurances using Rabin Karp alghoritm 
+    text_l=len(text)
+    pattern_l=len(pattern)
+    occurances=[]
+    if t_input=="I":
+        for i in range(text_l-pattern_l+1):
+            if text[i: i+pattern_l]==pattern:
+                occurances.append(i)
+    elif t_input=="F":
+        pattern1=sum(ord(pattern[i])*pow(10,pattern_l-i-1) for i in range(pattern_l))
+        text1=sum(ord(text[i])*pow(10,pattern_l-i-1) for i in range(pattern_l))
+        for i in range(text_l-pattern_l+1):
+            if text1==pattern1 and text[i:i +pattern_l]==pattern:
+                occurances.append(i)
+            if i<text_l-pattern_l:
+                text1=(text1-ord(text[i])*pow(10,pattern_l-1))*10+ord(text[i+pattern_l]) 
+                                        
+    # and return an iterable variable
+    return occurances
+    
 
-def get_occurrences(pattern, text):
-    p = 31
-    m, n = len(pattern), len(text)
-    p_pow = [pow(p, i) for i in range(m)]
-    pattern_hash = sum([ord(pattern[i]) * p_pow[m - 1 - i] for i in range(m)])
-    text_hash = sum([ord(text[i]) * p_pow[m - 1 - i] for i in range(m)])
-    matches = []
-    for i in range(n - m + 1):
-        if pattern_hash == text_hash:
-            if pattern == text[i:i + m]:
-                matches.append(i)
-        if i < n - m:
-            text_hash = (text_hash - ord(text[i]) * p_pow[m - 1]) * p + ord(text[i + m])
-    return matches
-
-
-if __name__ == '__main__':
-    input_data = read_input()
-    if input_data is None:
-        print('Error: could not read input')
-    else:
-        print_occurrences(get_occurrences(*input_data))
+# this part launches the functions
+if name == 'main':
+    print_occurrences(get_occurrences(*read_input()))
