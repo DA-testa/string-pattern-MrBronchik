90a12ef61352c24e80946fc0083c6c70f9606a07
MrBronchik <85889573+MrBronchik@users.noreply.github.com>
2023-04-16 23:56:02 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -3,29 +3,30 @@
 import sys
 
 def read_input():
-    # read the input choice
-    choice = input().strip().lower()
-    if choice == 'i':
-        # read from keyboard
-        pattern = input().strip()
-        text = input().strip()
-    elif choice == 'f':
-        # read from file
-        filename = input().strip()
-        try:
-            with open(filename, 'r') as f:
-                pattern = f.readline().strip()
-                text = f.readline().strip()
-        except FileNotFoundError:
-            print(f"Error: file '{filename}' not found", file=sys.stderr)
+    try:
+        # read the input choice
+        choice = input().strip().lower()
+        if choice == 'i':
+            # read from keyboard
+            pattern = input().strip()
+            text = input().strip()
+        elif choice == 'f':
+            # read from file
+            filename = input().strip()
+            try:
+                with open(filename, 'r') as f:
+                    pattern = f.readline().strip()
+                    text = f.readline().strip()
+            except FileNotFoundError:
+                print(f"Error: file '{filename}' not found", file=sys.stderr)
+                sys.exit(1)
+            except Exception as e:
+                print(f"Error: could not read file '{filename}': {e}", file=sys.stderr)
+                sys.exit(1)
+        else:
+            print(f"Error: invalid input choice '{choice}'", file=sys.stderr)
             sys.exit(1)
-        except Exception as e:
-            print(f"Error: could not read file '{filename}': {e}", file=sys.stderr)
-            sys.exit(1)
-    else:
-        print(f"Error: invalid input choice '{choice}'", file=sys.stderr)
-        sys.exit(1)
-    return pattern, text
+        return pattern, text
 
     except EOFError:
         return None
